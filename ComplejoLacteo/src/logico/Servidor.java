369a5249847755e1c855a4c86afc85d13a0b1a80
Leslie R <lesliern30@hotmail.com>
2021-04-09 20:52:24 -0400

    Servidor de programa

---
+++ ComplejoLacteo/src/logico/Servidor.java
@@ -0,0 +1,39 @@
+package logico;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Servidor extends Thread
+{
+  public static void main (String args[])
+  {
+    ServerSocket sfd = null;
+    try
+    {
+      sfd = new ServerSocket(7000);
+    }
+    catch (IOException ioe)
+    {
+      System.out.println("Comunicación rechazada."+ioe);
+      System.exit(1);
+    }
+
+    while (true)
+    {
+      try
+      {
+        Socket nsfd = sfd.accept();
+        System.out.println("Conexion aceptada\n");
+	    Flujo flujo = new Flujo(nsfd);
+	    Thread t = new Thread(flujo);
+        t.start();
+      }
+      catch(IOException ioe)
+      {
+        System.out.println("Error: "+ioe);
+      }
+    }
+  }
+}
+
